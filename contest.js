55001b2d2c3d9c4b8223d0afe6ee2b0727ffe22d
Sciforearth <67154128+deepanshubalyan01@users.noreply.github.com>
2023-08-09 01:28:39 +0530

    final contest settings

--- contest.js
+++ contest.js
@@ -21,6 +21,7 @@ const Ath = getAuth();
 const Pv = new GoogleAuthProvider()
 var longitude, latitude;
 
+
 const loader = document.getElementById('loader');
 
 const loginBttn = document.getElementById("login");
@@ -30,12 +31,15 @@ const loginBttn = document.getElementById("login");
 
 if (MA.currentUser?.id) {
     loginBttn.disabled = true
-    loginBttn.textContent = "Already Logged In  "
+    loginBttn.textContent = "Already Registered "
     loginBttn.style.color =  "#000000"
     loginBttn.style.backgroundColor = "#d5d5d5"
+
 }
 
+
 loginBttn.onclick = function () {
+   
     if (!MA.currentUser?.id) {
         // loader.classList.remove('hidden');
 
@@ -48,29 +52,15 @@ loginBttn.onclick = function () {
                 // loader.classList.add('hidden');
                 console.log(tk)
 
-                getLocation()
+               
                 MA.logIn(credentials).then((MUT) => {
-                    loginBttn.textContent = "already logged in"
+                    loginBttn.textContent = "Done"
 
+                        return MUT.functions.callFunction("registerForContest",{coords : [longitude,latitude]} ).then((rs) => {
+                                alert("Successfully registered!")
+                              })
 
-                    //       if (isImageSelected()) {
-                    //         compressAndEncodeImage().then((imageData) => {
-
-                    //           console.log(imageData);
-
-                    //           x.image =  imageData
-                    //           return MUT.functions.callFunction(fun, x).then((rs) => {
-                    //             alert("Successfully registered!")
-                    //           })
-
-                    //         })
-                    //         return
-                    //       }else{
-                    //         return  MUT.functions.callFunction(fun, x).then((rs) => {
-                    // alert("Successfully registered!")
-                    // })
-                    // }
-
+                  
 
 
                 })
@@ -96,44 +86,5 @@ function showPosition(position) {
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
     console.log(longitude + "..." + latitude)
+
 }
-
-
-
-
-function makeHttpRequest(url, method, data, callback) {
-var xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === XMLHttpRequest.DONE) {
-    if (xhr.status >= 200 && xhr.status < 300) {
-      // Request was successful
-      console.log("successfull call")
-      callback(null, xhr.responseText);
-    } else {
-      // Request failed
-      console.log("unsuccessfull call")
-      callback(new Error('Request failed'));
-    }
-  }
-};
-
-xhr.open(method, url, true);
-xhr.setRequestHeader('Content-Type', 'application/json'); // Set the appropriate content type if you're sending JSON data
-xhr.send(data ? JSON.stringify(data) : null);
-}
-
-// Example usage:
-const url = "https://us-east-1.aws.data.mongodb-api.com/app/analytics-wsxfv/endpoint/PetUs?page=contest"
-const method = 'GET'; // Change this to 'POST', 'PUT', 'DELETE', etc. if needed
-const data = {page : "PetUs contest"}; // Change this to your payload if you're making a POST or PUT request
-
-window.onload  = makeHttpRequest(url, method, data, function (error, response) {
-if (error) {
-  console.error('Error:', error);
-} else {
-  console.log('Response:', response);
-}
-});
-
-
--- shared.js
+++ shared.js
@@ -1,19 +1,22 @@
 function makeHttpRequest(page = "default") {
     var xhr = new XMLHttpRequest();
+    let callback = true
     const url = `https://us-east-1.aws.data.mongodb-api.com/app/analytics-wsxfv/endpoint/PetUs?page=page`
     xhr.onreadystatechange = function () {
       if (xhr.readyState === XMLHttpRequest.DONE) {
         if (xhr.status >= 200 && xhr.status < 300) {
           // Request was successful
           console.log("successfull call")
-          callback(null, xhr.responseText);
+        //  callback(null, xhr.responseText);
         } else {
           // Request failed
           console.log("unsuccessfull call")
-          callback(new Error('Request failed'));
+         // callback(new Error('Request failed'));
         }
       }
     };
+    let data = {}
+
     
     xhr.open("GET", url, true);
     xhr.setRequestHeader('Content-Type', 'application/json'); // Set the appropriate content type if you're sending JSON data
